6743cd84adc3a80759de7099a4ea2809919c1b81
neil patel <neil@myindianweddingapp.com>
2019-04-18 15:17:29 +0100

    Made test API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
-import num from './test';
-const x = 23
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = `aaaaa`;
+
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('cheese');
+
