a5c78c3f710ae707fe1254301e36270e45219182
neil patel <neil@myindianweddingapp.com>
2019-04-19 10:00:26 +0100

    Added search controller and state management

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,21 @@ import axios from 'axios';
 
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const state = {};
+
+const controlSearch = async () => {
+    const query = 'pizza'
+
+    if (query) {
+        state.search = new Search(query);
+        await state.search.getResults();
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 
